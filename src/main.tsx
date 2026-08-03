5d519d31b764dcaf3843625300c87ca0b2b5bedd
do-ift <ngovando261297@gmail.com>
2026-08-03 13:58:57 +0700

    add pwa

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({
+  onNeedRefresh() {
+    console.log("New content available");
+  },
+  onOfflineReady() {
+    console.log("App ready offline");
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
